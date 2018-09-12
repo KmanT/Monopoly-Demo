43c98035b69722c1b454458356ec01284677abd5
KmanT <kturske@gmail.com>
2018-09-11 22:13:34 -0400

    Second commit
    Changed the Property class a bit

--- src/srcpackage/Property.java
+++ src/srcpackage/Property.java
@@ -2,20 +2,24 @@ package srcpackage;
 
 public class Property extends Space{
 
+    private int propGroup;
     private int spaceOwnerID;
     private int houseCount;
     private boolean hasHotel;
 
-    public Property(String propName, double propPrice, double propRent, int spaceOwnerID, int houseCount, boolean hasHotel) {
+
+    public Property(String propName, double propPrice, double propRent, int propGroup, int spaceOwnerID, int houseCount, boolean hasHotel) {
         spaceName = propName;
         spacePrice = propPrice;
-        spaceRent = propPrice;
+        spaceRent = propRent;
+        this.propGroup = propGroup;
         this.spaceOwnerID = spaceOwnerID;
         this.houseCount = houseCount;
         this.hasHotel = hasHotel;
     }
 
     public Property() {
+        propGroup = -1; //the reason why it's not zero for the default is because I plan on using group 0 for the first group of spaces
         spaceOwnerID = 0;
         houseCount = 0;
         hasHotel = false;
