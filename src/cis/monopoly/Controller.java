e43230b3aa425b64517f65cd0162ee4bc8486453
KmanT <kturske@gmail.com>
2018-09-23 16:50:22 -0400

    Added the Game Play Scene
    This is where most of our work will lie. I have only added a menubar, but we'll use this as a starting point.

--- src/cis/monopoly/Controller.java
+++ src/cis/monopoly/Controller.java
@@ -6,6 +6,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
+import static com.sun.corba.se.impl.util.Utility.printStackTrace;
+
 /**
  * This class controls the UI and FXML Elements for the program.
  * All GUI related logic will go inside here
@@ -16,7 +18,7 @@ import javafx.stage.Stage;
 
 public class Controller {
 
-    public Button btnNewGame, btnExit, btnMainMenu;
+    public Button btnNewGame, btnExit, btnMainMenu, btnTwoPlayer;
 
     public void exitClick() {
         Stage stage = (Stage) btnExit.getScene().getWindow();
@@ -38,7 +40,8 @@ public class Controller {
     public void mainMenuClick() {
         Stage stage = (Stage) btnMainMenu.getScene().getWindow();
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource
+                    ("MainMenu.fxml"));
             stage.setScene(new Scene(root, 1200, 800));
         } catch (java.io.IOException e ) {
             AlertBox ioAlert = new AlertBox();
@@ -46,4 +49,30 @@ public class Controller {
         }
     }
 
+    public void startGameClick() {
+        Stage stage = (Stage) btnTwoPlayer.getScene().getWindow();
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource
+                    ("GamePlay.fxml"));
+            stage.setScene(new Scene(root, 1200, 800));
+        } catch (java.io.IOException e ) {
+            AlertBox ioAlert = new AlertBox();
+            ioAlert.display("ioException", "You have encountered an IO Exception");
+            printStackTrace();
+        }
+    }
+
+    /*
+    public void sceneChange(Button btn, String sceneName) {
+        Stage stage = (Stage) btn.getScene().getWindow();
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource
+                    (sceneName + ".fxml"));
+            stage.setScene(new Scene(root, 1200, 800));
+        } catch (java.io.IOException e ) {
+            AlertBox ioAlert = new AlertBox();
+            ioAlert.display("ioException", "You have encountered an IO Exception");
+        }
+    }
+    */
 }
